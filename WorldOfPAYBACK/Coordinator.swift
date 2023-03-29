d62dc1629ad6ed33ba31f61515714ba63a9af0cb
Felipe Andrade <andrade.mct@gmail.com>
2023-03-29 20:27:14 -0300

    First commit

---
+++ WorldOfPAYBACK/Coordinator.swift
@@ -0,0 +1,19 @@
+//
+//  Coordinator.swift
+//  WorldOfPAYBACK
+//
+//  Created by Felipe Andrade on 25/03/23.
+//
+
+import SwiftUI
+
+struct AppCoordinator: Scene {
+    
+    let rootView = ContentView()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
